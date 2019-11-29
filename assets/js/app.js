370bcd5a2d527790caa3f5af9595d0963c14425a
ryguy030286 <rsabo@luc.edu>
2019-11-29 16:20:43 -0600

    Boostrap js is finished.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
 // we accept a param e (for event) even though we're not using it. The event handler will automatically pass the event to the function. We could leave it out entirely since we're not using it, but I think it's helpful to see it.
 function myAppOnload(e) {
     $('.alert').alert();
+
+    //Bootstrap JS Tool Tip
+    $('[data-toggle="tooltip"]').tooltip()
+
 }
 
 document.addEventListener('onload', myAppOnload); // notice we do NOT call myAppOnload, we only pass the name of it. The event listener will call it (by using () after the name) when the event is triggered
 
 
-document.getElementById("contact").addEventListener("submit", (e) => {
-    e.preventDefault(); // prevents the event from taking the default action of sending the form as a postback event using GET
-});
+document.getElementById("contact").addEventListener("submit", validate);
 
 
 
@@ -17,28 +19,42 @@ var firstName = document.getElementById("first");
 var lastName = document.getElementById("last");
 var email = document.getElementById("email");
 var calendar = document.getElementById("calendar");
+var dropDown = document.getElementById("drop-down-feedback");
 
-function validate() {
-    if (firstName.value == "" || lastName.value == "" || email.value == "" || calendar.value == "") {
-        console.log("The input is empty.");
+
+function validate(e) {
+if (firstName.value == "" || lastName.value == "" || email.value == "" || calendar.value == "" || dropDown.value == "") {
+        console.log("Launch error message.");
+        document.getElementById("form-failed").classList.remove("d-none");
+        document.getElementById("form-success").classList.add("d-none");
+        e.preventDefault();
     } else {
-        console.log("The input is a value.");
+        console.log("Confirm validation is ok.");
+        document.getElementById("form-success").classList.remove("d-none");
+        document.getElementById("form-failed").classList.add("d-none");
+        e.preventDefault();
     }
 }
 
-var dropDown = document.getElementById("form-dropdown-test");
+//Bootstrap Toast Message
 
-/*
-var lastName = document.getElementById("last");
-var email = document.getElementById("email");
-//let reason = document.getElementById("form-dropdown-test");
-var date = document.getElementById("date").value;
+//Launches advertisement 5 seconds after page loads.
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.toast').toast('show');
+    }, 5000);
+});
+
+//Closes advertisement 30 seconds after page loads.
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.toast').toast('hide');
+    }, 30000);
+});
 
 
-function validate(){
-if (date == null){
-    console.log("it's empty, and The data in the input reads as " + date);
-}else{console.log("it has content and the data reads as " + date)}
-};
+//Boostrap Drop Down on Form
 
- */
+$('#form-dropdown-test a').on('click', function(){
+    $('#drop-down-feedback').val($(this).html());
+});
