ead805ceda95426d0e15cd892d88ab5695ecbaff
Ryan <rsabo@luc.edu>
2019-12-01 18:06:21 -0600

    added console log messages in various places.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 function myAppOnload(e) {
     $('.alert').alert();
 
-//Form Validation
+    //Form Validation
     var firstName = document.getElementById("first");
     var lastName = document.getElementById("last");
     var email = document.getElementById("email");
@@ -10,68 +10,70 @@ function myAppOnload(e) {
     var dropDown = document.getElementById("drop-down-feedback");
     var form = document.getElementById("#contact")
 
-            form.addEventListener('submit', e => {
-                e.preventDefault();
+    form.addEventListener('submit', e => {
+        e.preventDefault();
 
-                if (firstName.value == "" || lastName.value == "" || email.value == "" || calendar.value == "" || dropDown.value == "") {
-                    console.log("Launch error message.");
-                    document.getElementById("form-failed").classList.remove("d-none");
-                    document.getElementById("form-success").classList.add("d-none");
-                } else {
-                    console.log("Confirm validation is ok.");
-                    document.getElementById("form-success").classList.remove("d-none");
-                    document.getElementById("form-failed").classList.add("d-none");
-                    const form = document.getElementById("#contact");
+        if (firstName.value == "" || lastName.value == "" || email.value == "" || calendar.value == "" || dropDown.value == "") {
+            console.log("Launch error message.");
+            document.getElementById("form-failed").classList.remove("d-none");
+            document.getElementById("form-success").classList.add("d-none");
+        } else {
+            console.log("Confirm validation is ok.");
+            document.getElementById("form-success").classList.remove("d-none");
+            document.getElementById("form-failed").classList.add("d-none");
+            const form = document.getElementById("#contact");
 
-                    const method = form.getAttribute("method");
-                    const action = form.getAttribute("action");
-                    const submitForm = async (evt) => {
-                        return await fetch(action, {
-                            method, // shorthand for method: method
-                            mode: 'cors', // make sure you are running through HTTP:// and not file://
-                            data: new FormData(form) // see <https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData>
-                        });
-                    };
+            const method = form.getAttribute("method");
+            const action = form.getAttribute("action");
+            const submitForm = async (evt) => {
+                return await fetch(action, {
+                    method, // shorthand for method: method
+                    mode: 'cors', // make sure you are running through HTTP:// and not file://
+                    data: new FormData(form) // see <https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData>
+                });
+            };
 
-                console.log(submitForm());
-                console.log(' ');
-                console.log(submitForm().json());
-                // @todo handle submitForm Promise
-                // @see <https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Response_objects>
-                // @see <https://javascript.info/async>
-                // on a successful promise/submission, display a success alert
-                // on a failure promise/submission, display a failure (danger) alert
+        console.log(submitForm());
+        console.log(' ');
+        console.log(submitForm().json());
 
-            }});
+        // @todo handle submitForm Promise
+        // @see <https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Response_objects>
+        // @see <https://javascript.info/async>
+        // on a successful promise/submission, display a success alert
+        // on a failure promise/submission, display a failure (danger) alert
 
-            console.log("Outside of else statement.");
+    }});
 
-//Bootstrap Toast Message
-
-//Launches advertisement 5 seconds after page loads....
-    $(document).ready(function() {
-        setTimeout(function() {
-            $('.toast').toast('show');
-        }, 5000);
-    });
-
-//Closes advertisement 30 seconds after page loads.
-    $(document).ready(function() {
-        setTimeout(function() {
-            $('.toast').toast('hide');
-        }, 30000);
-    });
-
-
-//Boostrap Drop Down on Form
-
-//Code was modified from https://stackoverflow.com/questions/24620741/get-selected-item-value-from-bootstrap-dropdown-with-specific-id
-    $('#form-dropdown-test a').on('click', function(){
-        $('#drop-down-feedback').val($(this).html());
-    });
+    console.log("End of submit button function.  The function was loaded.");
 
 }
 
 document.addEventListener('onload', myAppOnload); // notice we do NOT call myAppOnload, we only pass the name of it. The event listener will call it (by using () after the name) when the event is triggered
 
+console.log("myApponLoad Loaded");
 
+
+//Bootstrap Toast Message
+
+//Launches advertisement 5 seconds after page loads....
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.toast').toast('show');
+    }, 5000);
+});
+
+//Closes advertisement 30 seconds after page loads.
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.toast').toast('hide');
+    }, 30000);
+});
+
+
+//Boostrap Drop Down on Form
+
+//Code was modified from https://stackoverflow.com/questions/24620741/get-selected-item-value-from-bootstrap-dropdown-with-specific-id
+$('#form-dropdown-test a').on('click', function(){
+    $('#drop-down-feedback').val($(this).html());
+});
