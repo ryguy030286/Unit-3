5f4906ad3029a6821e428238b4e91ccc205eb316
ryguy030286 <48996096+ryguy030286@users.noreply.github.com>
2019-12-03 12:17:55 -0600

    Added try and catch blocks to problem areas so error messages stop generating for now.  These may be stopping the script because they remain uncaught.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,102 +2,93 @@
 function myAppOnload(e) {
     $('.alert').alert();
 
-    console.log("Start of myAppOnLoad function.");
-
     let form = document.getElementById("contact")
+    try {
+        form.addEventListener('submit', e => {
+            e.preventDefault();
 
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-
-        //Form Validation
-        let firstName = document.getElementById("first");
-        let lastName = document.getElementById("last");
-        let email = document.getElementById("email");
-        let calendar = document.getElementById("calendar");
-        let dropDown = document.getElementById("drop-down-feedback");
-
-
-        if (firstName.value == "" || lastName.value == "" || email.value == "" || calendar.value == "" || dropDown.value == "") {
-            console.log("One of the fields are not filled out.");
-
-        } else {
-            console.log("All the fields are filled out.");
-
-            const form = document.getElementById("contact");
-
-            const method = form.getAttribute("method");
-            const action = form.getAttribute("action");
+            //Form Validation
+            let firstName = document.getElementById("first");
+            let lastName = document.getElementById("last");
+            let email = document.getElementById("email");
+            let calendar = document.getElementById("calendar");
+            let dropDown = document.getElementById("drop-down-feedback");
 
 
-            let data = $(form).serialize();
-            let options = {
-                method, // shorthand for method: method
-                mode: 'cors', // make sure you are running through HTTP:// and not file://
-                body: new FormData (form)// see <https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData>
-            }
+            if (firstName.value == "" || lastName.value == "" || email.value == "" || calendar.value == "" || dropDown.value == "") {
+                console.log("One of the fields are not filled out.");
 
-            /*
-            const submitForm = async (evt) => {
-                return await fetch(action, options)
+            } else {
+                console.log("All the fields are filled out.");
 
-                   .then(function(response){
-                    if (response.ok) {
-                        console.log("Form submission was successful, 200 code received.");
-                        document.getElementById("form-success").classList.remove("d-none");
-                        document.getElementById("form-failed").classList.add("d-none");
-                    }
-                })
-                    .catch(function(err){
-                    document.getElementById("form-failed").classList.remove("d-none");
-                    document.getElementById("form-success").classList.add("d-none");
-                    console.log("Received error message from form website after submitting.");
-                    console.log(response.status);
-                    console.error(err);
+                const form = document.getElementById("contact");
 
-                });
+                const method = form.getAttribute("method");
+                const action = form.getAttribute("action");
 
 
-             */
+                let data = $(form).serialize();
+                let options = {
+                    method, // shorthand for method: method
+                    mode: 'cors', // make sure you are running through HTTP:// and not file://
+                    body: new FormData(form)// see <https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData>
+                }
 
-            //Ryan's new test, original code block is commented out above.
-            const submitForm = async (evt) => {
-                return await fetch(action, options)
+                /*
+                const submitForm = async (evt) => {
+                    return await fetch(action, options)
 
-                    .then(function(response){
-                        if (response.status == 200) {
+                       .then(function(response){
+                        if (response.ok) {
                             console.log("Form submission was successful, 200 code received.");
                             document.getElementById("form-success").classList.remove("d-none");
                             document.getElementById("form-failed").classList.add("d-none");
                         }
                     })
-                    .catch(function(err){
+                        .catch(function(err){
                         document.getElementById("form-failed").classList.remove("d-none");
                         document.getElementById("form-success").classList.add("d-none");
                         console.log("Received error message from form website after submitting.");
+                        console.log(response.status);
                         console.error(err);
 
                     });
 
 
+                 */
 
+                //Ryan's new test, original code block is commented out above.
+                const submitForm = async (evt) => {
+                    return await fetch(action, options)
 
+                        .then(function (response) {
+                            if (response.status == 200) {
+                                console.log("Form submission was successful, 200 code received.");
+                                document.getElementById("form-success").classList.remove("d-none");
+                                document.getElementById("form-failed").classList.add("d-none");
+                            }
+                        })
+                        .catch(function (err) {
+                            document.getElementById("form-failed").classList.remove("d-none");
+                            document.getElementById("form-success").classList.add("d-none");
+                            console.log("Received error message from form website after submitting.");
+                            console.error(err);
 
-            };
+                        });
+                };
 
-                console.log(submitForm());
+                // @todo handle submitForm Promise
+                // @see <https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Response_objects>
+                // @see <https://javascript.info/async>
+                // on a successful promise/submission, display a success alert
+                // on a failure promise/submission, display a failure (danger) alert
 
-        // @todo handle submitForm Promise
-        // @see <https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Response_objects>
-        // @see <https://javascript.info/async>
-        // on a successful promise/submission, display a success alert
-        // on a failure promise/submission, display a failure (danger) alert
+            }
 
+        });  //End of method
+    }catch (e) {
+        console.log("There was an error on line 9.");
     }
-
-    });
-
-    console.log("myAppOnLoad Loaded");
-
     }
 
 //Recording and viewing page views
@@ -124,7 +115,16 @@ function addPageView() {
     let arr = [];
     if (pageViews && pageViews.length > 0) {
         // get the array stored in local storage at pageViewsKeyName
-        arr = JSON.parse(pageViews);
+       try {
+           arr = JSON.parse(pageViews);
+       }catch (e) {
+           console.log("Error on line 121 with the JSON parse.");
+       }
+
+       console.log("However, if this text shows that means the issues were caught and the script is " +
+           "continuing to execute.");
+
+
     }
 
     // now we're able to insert an item in the page view data
@@ -144,38 +144,35 @@ function addPageView() {
 }
 
 function listPageViews() {
-
 }
 
+    document.addEventListener('DOMContentLoaded', myAppOnload); // notice we do NOT call myAppOnload, we only pass the name of it. The event listener will call it (by using () after the name) when the event is triggered
 
-// window.onload = addPageView;
-
-document.addEventListener('DOMContentLoaded', myAppOnload); // notice we do NOT call myAppOnload, we only pass the name of it. The event listener will call it (by using () after the name) when the event is triggered
-console.log("After the document event listener");
-document.addEventListener('DOMContentLoaded', addPageView);
+//Loads method to record page urls and time stamp to local stoarge.
+    document.addEventListener('DOMContentLoaded', addPageView);
 
 
 //Bootstrap js
 
 //Toast Message
 //Launches advertisement 5 seconds after page loads....
-$(document).ready(function() {
-    setTimeout(function() {
-        $('.toast').toast('show');
-    }, 5000);
-});
+    $(document).ready(function () {
+        setTimeout(function () {
+            $('.toast').toast('show');
+        }, 5000);
+    });
 
 //Closes advertisement 30 seconds after page loads.
-$(document).ready(function() {
-    setTimeout(function() {
-        $('.toast').toast('hide');
-    }, 30000);
-});
+    $(document).ready(function () {
+        setTimeout(function () {
+            $('.toast').toast('hide');
+        }, 30000);
+    });
 
 
 //Drop Down on Form
 
 //Code was modified from https://stackoverflow.com/questions/24620741/get-selected-item-value-from-bootstrap-dropdown-with-specific-id
-$('#form-dropdown-test a').on('click', function(){
-    $('#drop-down-feedback').val($(this).html());
-});
+    $('#form-dropdown-test a').on('click', function () {
+        $('#drop-down-feedback').val($(this).html());
+    });
