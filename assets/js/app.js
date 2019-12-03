ff2b5f9b1d6944f01dd5acd58badc8f75a3676b3
sweith <steven.weith@gmail.com>
2019-12-02 21:15:57 -0600

    Added moment.js in html pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,13 +68,56 @@ function myAppOnload(e) {
 
     });
 
-    console.log("myApponLoad Loaded");
+    console.log("myAppOnLoad Loaded");
 
+    }
+
+//Recording and viewing page views
+
+/**
+ * Need to do:
+ * - Add a new page view & timestamp
+ * - list the page views & timestamps
+ */
+
+const pageViewsKeyName = "pageViews";
+
+/**
+ * Add the current page path + timestamp to the pageviews entry in local storage
+ */
+function addPageView() {
+    console.log("addPageView loaded")
+    /**
+     * In order to add a page view,
+     * we have to first check if there are any page views set
+     * and if not, then we need to create the array first
+     * afterward, or if the array already existed, we want to append to the array
+     */
+    let pageViews = localStorage.getItem(pageViewsKeyName);
+    let arr = [];
+    if (pageViews && pageViews.length > 0) {
+        // get the array stored in local storage at pageViewsKeyName
+        arr = JSON.parse(pageViews);
+    }
+
+    // now we're able to insert an item in the page view data
+    let newPageData = {
+        "path": window.location.pathname,
+        "timestamp": moment()
+    };
+
+    // now add new page data to the array
+    arr.push(newPageData);
+
+    // finally, we want to update our storage with the most up to date array
+    localStorage.setItem(pageViewsKeyName, arr);
 }
+
+// window.onload = addPageView;
 
 document.addEventListener('DOMContentLoaded', myAppOnload); // notice we do NOT call myAppOnload, we only pass the name of it. The event listener will call it (by using () after the name) when the event is triggered
 console.log("After the document event listener");
-
+document.addEventListener('DOMContentLoaded', addPageView);
 
 
 //Bootstrap js
