9a0b9558da3aafc6d9c37c45665e548cd88216db
ryguy030286 <48996096+ryguy030286@users.noreply.github.com>
2019-12-04 15:28:10 -0600

    Updated server response failure alert. Started listView function.

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,9 +30,6 @@ function myAppOnload(e) {
                 //Start for JS code from https://jsfiddle.net/seamusleahy/rxeuaatw/
                 var formEl = document.getElementById('contact');
 
-
-
-                //formEl.addEventListener('submit', function(event) {
                     // 1. Setup the request
                     // ================================
                     // 1.1 Headers
@@ -55,7 +52,8 @@ function myAppOnload(e) {
 
                     // 2. Make the request
                     // ================================
-                    var url = 'https://formspree.io/mpzwlbpr';
+                    //var url = 'https://formspree.io/mpzwlbpr';  //Alex's form.
+                      var url = 'https://formspree.io/xrgbpkyw';  //Ryan's form.
                     var fetchOptions = {
                         method: 'POST',
                         headers,
@@ -64,92 +62,22 @@ function myAppOnload(e) {
 
                     var responsePromise = fetch(url, fetchOptions);
 
-
                     // 3. Use the response
                     // ================================
                     responsePromise
                         .then(function (response) {
-                            if (response.status == 200) {
+                            if (response.status >= 200 && response.status < 300) {
                                 console.log("Form submission was successful, 200 code received.");
                                 document.getElementById("form-success").classList.remove("d-none");
                                 document.getElementById("form-failed").classList.add("d-none");
-                            }
-                        })
-                        .catch(function (err) {
-                            document.getElementById("form-server-error").classList.remove("d-none");
-                            document.getElementById("form-success").classList.add("d-none");
-                            console.log("Received error message from form website after submitting.");
-                            console.error(err);
+                            }else {
+                                document.getElementById("form-server-error").classList.remove("d-none");
+                                document.getElementById("form-success").classList.add("d-none");
+                                console.log("Received error message from form website after submitting.");
+                                console.error(err);
+                            }});
 
-                        });
-                    // 3.1 Convert the response into JSON-JS object.
-                    //     .then(function(response) {
-                    //         return response.json();
-                    //     })
-                    //     // 3.2 Do something with the JSON data
-                    //     .then(function(jsonData) {
-                    //         console.log(jsonData);
-                    //         document.getElementById('results').innerText =
-                    //             JSON.stringify(jsonData);
-                    //     });
-
-
-                    //event.preventDefault();
-                //});
                 //End to JS code from https://jsfiddle.net/seamusleahy/rxeuaatw/
-
-
-
-
-                let data = $(form).serialize();
-                let options = {
-                    method, // shorthand for method: method
-                    mode: 'cors', // make sure you are running through HTTP:// and not file://
-                    data // see <https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData>
-                };
-
-                /*
-                const submitForm = async (evt) => {
-                    return await fetch(action, options)
-
-                       .then(function(response){
-                        if (response.ok) {
-                            console.log("Form submission was successful, 200 code received.");
-                            document.getElementById("form-success").classList.remove("d-none");
-                            document.getElementById("form-failed").classList.add("d-none");
-                        }
-                    })
-                        .catch(function(err){
-                        document.getElementById("form-failed").classList.remove("d-none");
-                        document.getElementById("form-success").classList.add("d-none");
-                        console.log("Received error message from form website after submitting.");
-                        console.log(response.status);
-                        console.error(err);
-
-                    });
-
-
-                 */
-
-                /*Ryan's new test, original code block is commented out above.
-                const submitForm = async (evt) => {
-                    return await fetch(action, options)
-
-                        .then(function (response) {
-                            if (response.status == 200) {
-                                console.log("Form submission was successful, 200 code received.");
-                                document.getElementById("form-success").classList.remove("d-none");
-                                document.getElementById("form-failed").classList.add("d-none");
-                            }
-                        })
-                        .catch(function (err) {
-                            document.getElementById("form-failed").classList.remove("d-none");
-                            document.getElementById("form-success").classList.add("d-none");
-                            console.log("Received error message from form website after submitting.");
-                            console.error(err);
-
-                        });
-                };*/
 
                 // @todo handle submitForm Promise
                 // @see <https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Response_objects>
@@ -164,6 +92,7 @@ function myAppOnload(e) {
         console.log("There was an error on line 9.");
     }
 }
+
 
 //Recording and viewing page views
 
@@ -186,18 +115,19 @@ function addPageView() {
      * afterward, or if the array already existed, we want to append to the array
      */
     let pageViews = localStorage.getItem(pageViewsKeyName);
-    console.log("Local Storage Count: " + localStorage.length);
+
     let arr = [];
     if (pageViews && pageViews.length > 0) {
         // get the array stored in local storage at pageViewsKeyName
 
-        arr = JSON.parse(pageViews);
+        // //arr = JSON.parse(pageViews);
 
-
-        console.log("However, if this text shows that means the issues were caught and the script is " +
-            "continuing to execute.");
-
-
+        for (let obj in pageViews) {
+            console.log(obj);
+            console.log(pageViews);
+            arr = obj;
+        }
+    }
         // now we're able to insert an item in the page view data
         let newPageData = {
             "path": window.location.pathname,
@@ -206,23 +136,62 @@ function addPageView() {
 
         // now add new page data to the array
         arr.push(newPageData);
-        console.log("Array Length: " + arr.length);
 
         // arr.push(newPageData.path, newPageData.timestamp);
 
-
         // finally, we want to update our storage with the most up to date array
-        localStorage.setItem(pageViewsKeyName, arr);
-    }
+        //localStorage.setItem(pageViewsKeyName, arr);
 
-    function listPageViews() {
-    }
+        //Ryan edit.  The array needs to be converted back to a string for storage.
+        localStorage.setItem(pageViewsKeyName, JSON.stringify(arr));
+
+
+
 }
+
+
+//===============  Load the internal storage ========================
+function listPageViews(array) {
+    let table = document.getElementById("websiteLogsTable");
+    let tableBody = document.getElementById("logTableBody");
+    array.map(item => {
+        //creates the elements
+        let tr = document.createElement("<tr>");
+        let pageURL = document.createElement("<td>");
+        let timeStamp = document.createElement("<td>");
+
+        //adds values to the tags
+        pageURL.innerText = array[0].path;
+        timeStamp.innerText = array[0].timestamp;
+
+        //add the td tags to the TR
+        tr.appendChild(pageURL);
+        tr.appendChild(timeStamp);
+
+        //add the TR to the table
+        tableBody.appendChild(tr);
+    });
+}
+//============== End of load internal storage ======================
+
+
+
 
     document.addEventListener('DOMContentLoaded', myAppOnload); // notice we do NOT call myAppOnload, we only pass the name of it. The event listener will call it (by using () after the name) when the event is triggered
 
-//Loads method to record page urls and time stamp to local stoarge.
-    document.addEventListener('DOMContentLoaded', addPageView);
+//Loads method to record page urls and time stamp to local storage.
+    //document.addEventListener('DOMContentLoaded', addPageView);
+
+//Ryan's addPageView call
+window.onload = addPageView;
+
+
+let testArray = [{path:"Sherdog.com", timestamp: "10.00am"}, {path:"google.com", timestamp: "10:30am"}];
+listPageViews(testArray);
+
+
+
+
 
 
 //Bootstrap js
@@ -249,5 +218,4 @@ function addPageView() {
     $('#form-dropdown-test a').on('click', function () {
         $('#drop-down-feedback').val($(this).html());
     });
-
 
